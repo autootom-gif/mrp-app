4f62b6d229900f0054e46e50db7a147ec43f1172
autootom-gif <autootom@gmail.com>
2025-08-14 13:02:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,69 +1,97 @@
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from sqlalchemy import func
+import os
 
 app = Flask(__name__)
-app.config.from_object(Config)
+
+# ---------------------
+# Konfiguracja bazy
+# ---------------------
+DB_HOST = "dpg-d2e5n4ggjchc73e2e6i0-a"
+DB_PORT = "5432"
+DB_NAME = "magazyn_wwo2"
+DB_USER = "abc"
+DB_PASS = "xruiiXquETHElmravuoxAXv5wUMuzs2l"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
-# --- MODELE ---
+# ---------------------
+# Modele bazy danych
+# ---------------------
 class Magazyn(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nazwa_produktu = db.Column(db.String(100), nullable=False)
-    ilosc = db.Column(db.Integer, default=0)
+    ilosc = db.Column(db.Integer, nullable=False)
 
 class BOM(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    produkt_finalny = db.Column(db.String(100), nullable=False)
+    produkt_id = db.Column(db.Integer, db.ForeignKey('magazyn.id'))
     komponent = db.Column(db.String(100), nullable=False)
-    ilosc = db.Column(db.Integer, default=0)
+    ilosc = db.Column(db.Integer, nullable=False)
 
 class Produkcja(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    produkt_finalny = db.Column(db.String(100), nullable=False)
-    ilosc = db.Column(db.Integer, default=0)
+    produkt_id = db.Column(db.Integer, db.ForeignKey('magazyn.id'))
+    ilosc = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String(50), default="W trakcie")
 
-# --- ROUTES ---
+# ---------------------
+# Widoki frontend
+# ---------------------
 @app.route('/')
 def index():
-    return "MRP App działa!"
+    return render_template('index.html')
 
-# Magazyn
-@app.route('/magazyn', methods=['GET'])
-def get_magazyn():
+@app.route('/magazyn/view')
+def view_magazyn():
+    produkty = Magazyn.query.all()
+    return render_template('magazyn.html', produkty=produkty)
+
+@app.route('/bom/view')
+def view_bom():
+    bom = BOM.query.all()
+    return render_template('bom.html', bom=bom)
+
+@app.route('/produkcja/view')
+def view_produkcja():
+    produkcja = Produkcja.query.all()
+    return render_template('produkcja.html', produkcja=produkcja)
+
+# ---------------------
+# API endpoints (JSON)
+# ---------------------
+@app.route('/api/magazyn', methods=['GET'])
+def api_magazyn():
     produkty = Magazyn.query.all()
     return jsonify([{'id': p.id, 'nazwa_produktu': p.nazwa_produktu, 'ilosc': p.ilosc} for p in produkty])
 
-@app.route('/magazyn', methods=['POST'])
-def add_magazyn():
+@app.route('/api/magazyn', methods=['POST'])
+def api_add_magazyn():
     data = request.get_json()
-    produkt = Magazyn(nazwa_produktu=data['nazwa_produktu'], ilosc=data.get('ilosc', 0))
-    db.session.add(produkt)
-    db.session.commit()
-    return jsonify({'message': 'Produkt dodany'}), 201
-
-# BOM
-@app.route('/bom', methods=['GET'])
-def get_bom():
-    bom = BOM.query.all()
-    return jsonify([{'id': b.id, 'produkt_finalny': b.produkt_finalny, 'komponent': b.komponent, 'ilosc': b.ilosc} for b in bom])
-
-@app.route('/bom', methods=['POST'])
-def add_bom():
-    data = request.get_json()
-    b = BOM(produkt_finalny=data['produkt_finalny'], komponent=data['komponent'], ilosc=data.get('ilosc',1))
-    db.session.add(b)
-    db.session.commit()
-    return jsonify({'message':'BOM dodany'}), 201
-
-# Produkcja
-@app.route('/produkcja', methods=['POST'])
-def add_produkcja():
-    data = request.get_json()
-    p = Produkcja(produkt_finalny=data['produkt_finalny'], ilosc=data.get('ilosc',1))
+    p = Magazyn(nazwa_produktu=data['nazwa_produktu'], ilosc=data['ilosc'])
     db.session.add(p)
     db.session.commit()
-    return jsonify({'message':'Produkcja dodana'}), 201
+    return jsonify({'status': 'ok', 'id': p.id})
 
+@app.route('/api/bom', methods=['GET'])
+def api_bom():
+    bom = BOM.query.all()
+    return jsonify([{'id': b.id, 'produkt_id': b.produkt_id, 'komponent': b.komponent, 'ilosc': b.ilosc} for b in bom])
+
+@app.route('/api/produkcja', methods=['GET'])
+def api_produkcja():
+    prod = Produkcja.query.all()
+    return jsonify([{'id': p.id, 'produkt_id': p.produkt_id, 'ilosc': p.ilosc, 'status': p.status} for p in prod])
+
+# ---------------------
+# Uruchomienie
+# ---------------------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Tworzenie tabel, jeśli nie istnieją
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
