6f99ce0abe305cd76a0ef4ef16b4a2968d413acb
autootom-gif <autootom@gmail.com>
2025-08-14 13:09:22 +0200

    Update models.py

--- models.py
+++ models.py
@@ -2,32 +2,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Produkt(db.Model):
+class Magazyn(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nazwa = db.Column(db.String(100), unique=True, nullable=False)
-    stan = db.Column(db.Integer, default=0)
-
-class Komponent(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nazwa = db.Column(db.String(100), unique=True, nullable=False)
+    nazwa_produktu = db.Column(db.String(100), nullable=False)
+    ilosc = db.Column(db.Integer, default=0)
 
 class BOM(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    produkt_id = db.Column(db.Integer, db.ForeignKey('produkt.id'), nullable=False)
-    komponent_id = db.Column(db.Integer, db.ForeignKey('komponent.id'), nullable=False)
-    ilosc = db.Column(db.Integer, nullable=False)
-    produkt = db.relationship('Produkt', backref=db.backref('bom', lazy=True))
-    komponent = db.relationship('Komponent', backref=db.backref('bom', lazy=True))
-
-class Przyjecie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    komponent_id = db.Column(db.Integer, db.ForeignKey('komponent.id'), nullable=False)
-    ilosc = db.Column(db.Integer, nullable=False)
-    komponent = db.relationship('Komponent', backref=db.backref('przyjecia', lazy=True))
+    produkt = db.Column(db.String(100), nullable=False)
+    komponent = db.Column(db.String(100), nullable=False)
+    ilosc = db.Column(db.Integer, default=0)
 
 class Produkcja(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    produkt_id = db.Column(db.Integer, db.ForeignKey('produkt.id'), nullable=False)
-    ilosc = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.String(50), default='planowane')
-    produkt = db.relationship('Produkt', backref=db.backref('produkcja', lazy=True))
+    produkt = db.Column(db.String(100), nullable=False)
+    ilosc_produkcji = db.Column(db.Integer, default=0)
