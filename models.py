5256b443609c1c0e1d6abba642f30ce4f3a0922c
autootom-gif <autootom@gmail.com>
2025-08-14 13:19:33 +0200

    Update models.py

--- models.py
+++ models.py
@@ -4,16 +4,16 @@ db = SQLAlchemy()
 
 class Magazyn(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nazwa_produktu = db.Column(db.String(100), nullable=False)
-    ilosc = db.Column(db.Integer, default=0)
-
-class BOM(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    produkt = db.Column(db.String(100), nullable=False)
-    komponent = db.Column(db.String(100), nullable=False)
-    ilosc = db.Column(db.Integer, default=0)
+    nazwa = db.Column(db.String(100), nullable=False)
+    ilosc = db.Column(db.Integer, nullable=False)
 
 class Produkcja(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    nazwa_produktu = db.Column(db.String(100), nullable=False)
+    ilosc = db.Column(db.Integer, nullable=False)
+
+class Bom(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     produkt = db.Column(db.String(100), nullable=False)
-    ilosc_produkcji = db.Column(db.Integer, default=0)
+    komponent = db.Column(db.String(100), nullable=False)
+    ilosc = db.Column(db.Integer, nullable=False)
