8e41541bc1047cba1970292c066edfeaf928c0bf
autootom-gif <autootom@gmail.com>
2025-08-14 12:50:25 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'mrp.db')
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{os.getenv('DB_USER','abc')}:{os.getenv('DB_PASSWORD','TwojeHaslo')}"
+        f"@{os.getenv('DB_HOST','dpg-d2e5n4ggjchc73e2e6i0-a')}:{os.getenv('DB_PORT','5432')}/{os.getenv('DB_NAME','magazyn_wwo2')}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'supersecretkey'
